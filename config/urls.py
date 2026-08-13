472b94033cd36badb36ef9eae4a9888a2b8e4ae0
Ricard0000000 <95ricard0.c0rrea@gmail.com>
2026-08-13 10:59:47 -0300

    Corrigindo urls de media e forçando imagens

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from django.contrib.auth import views as auth_views
-
 from django.conf import settings
-from django.conf.urls.static import static
 
 from produtos.views import (
     home,
@@ -210,10 +209,9 @@ urlpatterns = [
         excluir_produto,
         name='excluir_produto'
     ),
-]
 
-# --------------------------------------------------------------------------
-# 🖼️ SERVIR ARQUIVOS DE MÍDIA E ESTÁTICOS
-# --------------------------------------------------------------------------
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # --------------------------------------------------------------------------
+    # 🖼️ ROTA FORÇADA PARA MÍDIA NO RENDER (DEBUG=False)
+    # --------------------------------------------------------------------------
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
